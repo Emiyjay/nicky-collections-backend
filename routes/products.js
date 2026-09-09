90a28763d6200b824a26df89a07e05c29034629f
EMMANUEL JOHN <emmanueljohn98878@gmail.com>
2026-09-09 07:21:37 +0100

    fix: support slug and legacy product URLs

--- routes/products.js
+++ routes/products.js
@@ -7,7 +7,11 @@ const { protect } = require('../middleware/auth');
 router.get('/', getProducts);
 router.get('/featured', getFeatured);
 router.get('/slug/:slug', getProductBySlug);
-router.get('/:id', getProduct);
+router.get('/:id', (req, res, next) => {
+  if (/^[a-f\d]{24}$/i.test(req.params.id)) return getProduct(req, res, next);
+  req.params.slug = req.params.id;
+  return getProductBySlug(req, res, next);
+});
 router.post('/:id/reviews', protect, addReview);
 
 module.exports = router;

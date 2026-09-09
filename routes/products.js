3a50bb11c2e77d5ccd992962f70c5ff1bb45e1f9
EMMANUEL JOHN <emmanueljohn98878@gmail.com>
2026-09-09 07:59:48 +0100

    feat: expose product SEO and related routes

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getProduct, getFeatured, addReview } = require('../controllers/productController');
+const {
+  getProducts,
+  getProduct,
+  getProductBySlug,
+  getRelatedProducts,
+  getFeatured,
+  addReview
+} = require('../controllers/productController');
 const { protect } = require('../middleware/auth');
 
 router.get('/', getProducts);
 router.get('/featured', getFeatured);
+router.get('/slug/:slug', getProductBySlug);
+router.get('/:id/related', getRelatedProducts);
 router.get('/:id', getProduct);
 router.post('/:id/reviews', protect, addReview);
 

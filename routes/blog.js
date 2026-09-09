1fe27c31d368184ae16c1bd45089d74f6abbb843
EMMANUEL JOHN <emmanueljohn98878@gmail.com>
2026-09-09 07:11:54 +0100

    feat(cms): add public and admin blog routes

---
+++ routes/blog.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getPosts,
+  getPost,
+  adminGetPosts,
+  adminCreatePost,
+  adminUpdatePost,
+  adminDeletePost
+} = require('../controllers/blogController');
+const { protect, adminOnly } = require('../middleware/auth');
+
+router.get('/', getPosts);
+router.get('/:slug', getPost);
+
+router.get('/admin/all', protect, adminOnly, adminGetPosts);
+router.post('/admin', protect, adminOnly, adminCreatePost);
+router.put('/admin/:id', protect, adminOnly, adminUpdatePost);
+router.delete('/admin/:id', protect, adminOnly, adminDeletePost);
+
+module.exports = router;
